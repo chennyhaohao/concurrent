a2993c56958483b5173f34d43005bfbc87bd5a50
None <None>
2018-04-15 08:02:59 -0700

    Cleaner reports stats

--- cleaner.c
+++ cleaner.c
@@ -7,15 +7,38 @@
 
 #include "./shm.h"
 
+#define ITEM_NUM 20
+
 int       shm_id;
 key_t     mem_key;
 int       *shm_ptr;
+
+struct item_stat {
+	int item_id;
+	int frequency;
+	float revenue;
+};
+
+int comp(const void *elem1, const void *elem2) {
+	return ((struct item_stat *)elem1)->frequency - ((struct item_stat *)elem2)->frequency;
+}
 
 int main() {
 
 	struct order o;
 	int nread;
 	FILE * db_fp;
+	int total_wtime = 0, c_count = 0;
+	float revenue = 0.0;
+
+	struct item_stat stats[ITEM_NUM];
+
+	int i;
+	for (i = 0; i<ITEM_NUM; i++) {
+		stats[i].item_id = i+1;
+		stats[i].frequency = 0;
+		stats[i].revenue = 0.0;
+	}
 
 	if ( (db_fp = fopen("./db.bin", "r")) < 0 ) {
     	perror("fopen");
@@ -25,8 +48,26 @@ int main() {
     while(1) {
     	nread = fread(&o, sizeof(struct order), 1, db_fp);
     	if (nread == 0) break;
-    	printf("Client id: %d, item id:%d price: %f, waiting time:%d\n", o.client_id, o.item_id,
+    	printf("Client id: %d, item id:%d, money spent: %f, waiting time:%d\n", o.client_id, o.item_id,
     		o.price, o.waiting_time);
+    	total_wtime += o.waiting_time;
+    	revenue += o.price;
+    	stats[o.item_id - 1].frequency ++;
+    	stats[o.item_id - 1].revenue += o.price;
+    	c_count ++;
+    }
+
+    printf("Total number of customers: %d\n", c_count);
+    printf("Revenue of the day: %f\n", revenue);
+    if (c_count>0) {
+    	printf("Average waiting time: %d\n", total_wtime/c_count);
+    }
+
+    qsort(stats, ITEM_NUM, sizeof(struct item_stat), comp);
+
+    for (i = 0; i<5; i++) {
+    	struct item_stat s = stats[ITEM_NUM - i - 1];
+    	printf("ID: %d, frequency: %d, revenue: %f\n", s.item_id, s.frequency, s.revenue);
     }
 
     fclose(db_fp);
